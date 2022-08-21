73404afc7e31f0213b8bf57a5eb53c58081d41e2
reymond <reymondgomera9@gmail.com>
2022-08-21 15:16:13 +0800

    Create add/get course routes and functions to handle riasec/strand recommendation and fetch course description fulfillments

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,5 +4,7 @@ const adminController = require('../controller/adminController');
 
 router.get('/feedbacks', authorization, adminController.admin_getFeedbacks_get);
 router.post('/feedback', adminController.admin_addFeedback_post);
+router.get('/courses', authorization, adminController.admin_getCourses_get);
+router.post('/course', authorization, adminController.admin_addCourse_post);
 
 module.exports = router;
--- routes/fullfillmentRoutes.js
+++ routes/fullfillmentRoutes.js
@@ -1,4 +1,5 @@
 const { WebhookClient, Payload } = require('dialogflow-fulfillment');
+const Course = require('../models/Course');
 
 module.exports = app => {
    app.post('/', async (req, res) => {
@@ -7,9 +8,10 @@ module.exports = app => {
       console.log('\n intent = ', agent.intent);
       console.log('paramters = ', JSON.stringify(agent.parameters));
       console.log('context = ', JSON.stringify(agent.contexts));
-      console.log('messages = ', JSON.stringify(agent.consoleMessages) + '\n');
+      console.log('messages = ', JSON.stringify(agent.consoleMessages));
       console.log('query = ', agent.query); // user quiery
-      console.log('type of query = ', typeof agent.query);
+      console.log('type of query = ', typeof agent.query + '\n');
+
       // Utilities funcions
       const capitalizeFirstLetter = string => {
          return string.charAt(0).toUpperCase() + string.slice(1);
@@ -20,7 +22,34 @@ module.exports = app => {
       };
 
       // intent funcions handler
-      const handleGetAge = () => {
+      const handleGetName = agent => {
+         const userQuery = agent.query;
+
+         // check if contains digit
+         if (/\d/.test(userQuery)) {
+            agent.add(' ');
+            // You cannot have number in your name. Please tell your name again. Thank you 😊.
+            // There can't be a number in your name. Please repeat your name for me. Thank you 😊.
+            agent.setFollowupEvent('GET_NAME_WITH_NUMBER_FALLBACK');
+         } else {
+            const payload = {
+               quick_replies: [
+                  {
+                     text: 'Yes',
+                     payload: 'n/a',
+                  },
+                  {
+                     payload: 'n/a',
+                     text: 'No',
+                  },
+               ],
+            };
+            agent.consoleMessages.forEach(message => agent.add(message));
+            agent.add(new Payload(agent.UNSPECIFIED, payload, { rawPayload: true, sendAsMessage: true }));
+         }
+      };
+
+      const handleGetAge = agent => {
          if (agent.parameters.hasOwnProperty('age')) {
             if (agent.parameters.age <= 0) {
                agent.add(' '); // adding dummy text to avoid error -> No responses defined for platform: null
@@ -49,37 +78,20 @@ module.exports = app => {
          agent.consoleMessages.forEach(message => agent.add(message));
       };
 
-      const handleSampleRecommend = agent => {
-         // console.log('parameters = ', agent.parameters);
-         // console.log('context = ', JSON.stringify(agent.contexts));
+      const handleFallbackExceedTriggerLimit = agent => {
+         const contexts = agent.contexts;
 
-         // context based on the event name - ex : sample_recommend, which contains paramaters sent from front end
-         const context = agent.contexts.filter(el => el.name === 'sample_recommend'); // get the specific context
-         const parameters = context[0].parameters;
-         console.log('context = ', context);
-         console.log('paramters = ', context[0].parameters);
+         // clear all context by setting lifespan to zero (0)
+         contexts.forEach(context => {
+            agent.setContext({ name: context.name, lifespan: 0 });
+         });
 
-         agent.add(
-            `what hello ${parameters.name} fullfillment passed paramters , you are a ${parameters.riasec} ${parameters.sampleArray[0]} ${parameters.sampleArray[1]}`
-         );
-         n;
-         // the parameter  passsed  from front to dialogflow will be avaiable in the context
-         // the agent.parameters is empty because the paramaters passed is in the context
-         // dialogflow automatically create context based on the event name which contains the context you passed from front end
-         // dialogflow does not store the context passed from front-end to agent.parameters instead in the agent.context
-
-         // send custom payload
-         const payload = {
-            sample1: 'value',
-            sample2: 2,
-         };
-
-         agent.add(new Payload(agent.UNSPECIFIED, payload, { rawPayload: true, sendAsMessage: true }));
+         agent.consoleMessages.forEach(message => agent.add(message));
       };
 
-      const handleRiasecRecommendation = agent => {
+      const handleRiasecRecommendation = async agent => {
          // context based on the event name - ex : sample_recommend, which contains paramaters sent from front end
-         // the parameter  passsed  from front to dialogflow will be avaiable in the context
+         // the parameter  passsed  from front-end to dialogflow will be avaiable in the context
          // the agent.parameters is empty because the paramaters passed is in the context
          // dialogflow automatically create context based on the event name which contains the context you passed from front-end
          // dialogflow does not store the context passed from front-end to agent.parameters instead in the agent.context
@@ -87,29 +99,156 @@ module.exports = app => {
          const context = agent.contexts.filter(el => el.name === 'riasec_recommendation'); // get the specific context
          const parameters = context[0].parameters;
 
-         const riasecAreas = {
-            realistic: 'who are often good at mechanical or athletic jobs.',
-            investigative: 'who like to watch, learn, analyze and solve problems.',
-            artistic: 'like to work in unstructured situations where you can use your creativity.',
-            social: 'who like to work with other people, rather than things.',
-            enterprising: 'who like to work with others and enjoy persuading and and performing.',
-            conventional: 'who are very detail oriented, organized and like to work with data.',
-         };
+         console.log(parameters['0'][0], ' = ', parameters['0'][1]);
+         console.log(parameters['1'][0], ' = ', parameters['1'][1]);
+         console.log(parameters['2'][0], ' = ', parameters['2'][1]);
 
-         const riasecAreasIdentify = `
-         Now. I already know the things you are interested in. You are ${getAandAn(parameters['0'][0])} “${capitalizeFirstLetter(
-            parameters['0'][0]
-         )}”, “${capitalizeFirstLetter(parameters['1'][0])}” and “${capitalizeFirstLetter(parameters['2'][0])}” person.`;
+         if (!parameters['0'][1] && !parameters['1'][1] && !parameters['2'][1]) {
+            // student does not have any interest among those questions whtih riasec scores is all zero (0)
+            agent.add(
+               `It looks like you didn't have any interest in the things that I had asked you. 
+                Unfortunately 😓, because of that, I can't recommend you any degree programs at the moment. 
+                Instead, I can show you my recommendation based on your senior high school strand 😊.`
+            );
+            // trigger intent to show recommendation based on strand
+         } else {
+            // if riasec scores are not all zero
+            const riasecAreas = {
+               realistic: 'who are often good at mechanical or athletic jobs.',
+               investigative: 'who like to watch, learn, analyze and solve problems.',
+               artistic: 'like to work in unstructured situations where you can use your creativity.',
+               social: 'who like to work with other people, rather than things.',
+               enterprising: 'who like to work with others and enjoy persuading and and performing.',
+               conventional: 'who are very detail oriented, organized and like to work with data.',
+            };
 
-         const riasecAreasDescription = `
-         You’re ${getAandAn(parameters['0'][0])} "${capitalizeFirstLetter(parameters['0'][0])}" person ${riasecAreas[parameters['0'][0]]} 
-         You’re also ${getAandAn(parameters['1'][0])} "${capitalizeFirstLetter(parameters['1'][0])}" person, ${riasecAreas[parameters['1'][0]]} 
-         Lastly, I found out that your are ${getAandAn(parameters['2'][0])} "${capitalizeFirstLetter(parameters['2'][0])}" person, ${
-            riasecAreas[parameters['2'][0]]
-         }`;
+            const riasecAreasIdentify = `
+            Now. I already know the things you are interested in. You are ${getAandAn(parameters['0'][0])} “${capitalizeFirstLetter(
+               parameters['0'][0]
+            )}”, “${capitalizeFirstLetter(parameters['1'][0])}” and “${capitalizeFirstLetter(parameters['2'][0])}” person.`;
 
-         agent.add(riasecAreasIdentify);
-         agent.add(riasecAreasDescription);
+            const riasecAreasDescription = `
+            You’re ${getAandAn(parameters['0'][0])} "${capitalizeFirstLetter(parameters['0'][0])}" person ${riasecAreas[parameters['0'][0]]} 
+            You’re also ${getAandAn(parameters['1'][0])} "${capitalizeFirstLetter(parameters['1'][0])}" person, ${riasecAreas[parameters['1'][0]]} 
+            Lastly, I found out that your are ${getAandAn(parameters['2'][0])} "${capitalizeFirstLetter(parameters['2'][0])}" person, ${
+               riasecAreas[parameters['2'][0]]
+            }`;
+
+            const riasec1 = parameters['0'][0].toUpperCase(); // highest score riasec area
+            const riasec2 = parameters['1'][0].toUpperCase(); // second or same with higest
+            const riasec3 = parameters['2'][0].toUpperCase(); // third or same with higest
+            const toFilterCourse = [parameters['0'][0].toUpperCase()];
+            const payload = { basis: 'riasec' };
+
+            console.log('\n1.', riasec1);
+            console.log('2.', riasec2);
+            console.log('3.', riasec3);
+
+            // Add into the toFilterCourse the riasec area that is euqal to the highest score riasec area
+            // value of toFilterCourse are the riasec area
+            if (parameters['0'][1] === parameters['1'][1]) toFilterCourse.push(riasec2);
+            if (parameters['0'][1] === parameters['2'][1]) toFilterCourse.push(riasec3);
+
+            console.log('filterObject = ', toFilterCourse);
+
+            // fetch courses or degree program based on toFilterCourse
+            try {
+               const riasecBasedRecommendedCourses = await Course.find({ riasec_area: { $in: toFilterCourse } }).sort({ name: 'asc' });
+               const riasecBasedRecommendedCoursesNames = riasecBasedRecommendedCourses.map(course => course.name);
+               payload.riasec_recommended_courses = riasecBasedRecommendedCoursesNames;
+            } catch (err) {
+               console.error(err.message);
+            }
+
+            agent.add(riasecAreasIdentify);
+            agent.add(riasecAreasDescription);
+            agent.add(new Payload(agent.UNSPECIFIED, payload, { rawPayload: true, sendAsMessage: true })); // passed custom payload
+         }
+      };
+
+      const handleStrandRecommendation = async agent => {
+         const context = agent.contexts.filter(el => el.name === 'strand_recommendation'); // get the specific context
+         const parameters = context[0].parameters;
+         const toFilterStrand = [parameters['strand'].toUpperCase()];
+         const payload = { basis: 'strand' };
+
+         console.log('toFilterStrand = ', toFilterStrand);
+
+         try {
+            const strandBasedRecommendedCourses = await Course.find({ strand: { $in: toFilterStrand } }).sort({ name: 'asc' });
+            const strandBasedRecommendedCoursesNames = strandBasedRecommendedCourses.map(course => course.name);
+            console.log('strandBasedRecommendedCourses = ', strandBasedRecommendedCourses);
+            payload.strand_recommended_courses = strandBasedRecommendedCoursesNames;
+         } catch (err) {
+            console.error(err.message);
+         }
+
+         agent.add(new Payload(agent.UNSPECIFIED, payload, { rawPayload: true, sendAsMessage: true })); // passed custom payload
+      };
+
+      const handleGetRiasecRecommendationCourseInfo = async agent => {
+         const context = agent.contexts.filter(el => el.name === 'get_riasec_recommendation_course_info'); // get the specific context
+         const parameters = context[0].parameters;
+         const courseToLookup = parameters.course_to_lookup;
+
+         try {
+            const course = await Course.findOne({ name: courseToLookup });
+            if (course) {
+               const payload = {
+                  quick_replies: [
+                     {
+                        text: 'I am satisfied',
+                        payload: 'ISWANT_STRAND_RECOMMENDATION',
+                     },
+                     {
+                        text: "I'd like to know other degree programs",
+                        payload: 'ISLEARN_RIASEC_RECOMMENDED_COURSES_YES',
+                     },
+                  ],
+               };
+
+               agent.add(course.description);
+               agent.add(new Payload(agent.UNSPECIFIED, payload, { rawPayload: true, sendAsMessage: true }));
+            } else {
+               agent.add(' ');
+               agent.setFollowupEvent('ISLEARN_RIASEC_RECOMMENDED_COURSES_YES');
+            }
+         } catch (error) {
+            agent.add('Sorry. I am having trouble 🤕. I was unable to look up the degree programs information at the moment.');
+         }
+      };
+
+      const handleGetStrandRecommendationCourseInfo = async agent => {
+         const context = agent.contexts.filter(el => el.name === 'get_strand_recommendation_course_info'); // get the specific context
+         const parameters = context[0].parameters;
+         const courseToLookup = parameters.course_to_lookup;
+
+         try {
+            const course = await Course.findOne({ name: courseToLookup });
+            if (course) {
+               const payload = {
+                  quick_replies: [
+                     {
+                        text: 'I am satisfied',
+                        payload: 'END_CONVERSATION', // temporary value
+                     },
+                     {
+                        text: "I'd like to know other degree programs",
+                        payload: 'ISLEARN_STRAND_RECOMMENDED_COURSES_YES',
+                     },
+                  ],
+               };
+
+               console.log(course);
+               agent.add(course.description);
+               agent.add(new Payload(agent.UNSPECIFIED, payload, { rawPayload: true, sendAsMessage: true }));
+            } else {
+               agent.add(' ');
+               agent.setFollowupEvent('ISLEARN_STRAND_RECOMMENDED_COURSES_YES');
+            }
+         } catch (error) {
+            agent.add('Sorry. I am having trouble 🤕. I was unable to look up the degree programs information at the moment.');
+         }
       };
 
       const checkUncertainty = agent => {
@@ -136,10 +275,14 @@ module.exports = app => {
       // set IntentMap with intent name and a intent function handler to run for that intent
       let intentMap = new Map();
       intentMap.set('Default Welcome Intent', handleWelcome);
+      intentMap.set('get-name', handleGetName);
       intentMap.set('get-age', handleGetAge);
       intentMap.set('riasec-start', handleRiasecStart);
-      intentMap.set('sample-recommend', handleSampleRecommend);
       intentMap.set('riasec-recommendation', handleRiasecRecommendation);
+      intentMap.set('strand-recommendation', handleStrandRecommendation);
+      intentMap.set('fallback-exceed-trigger-limit', handleFallbackExceedTriggerLimit);
+      intentMap.set('get-riasec-recommendation-course-info', handleGetRiasecRecommendationCourseInfo);
+      intentMap.set('get-strand-recommendation-course-info', handleGetStrandRecommendationCourseInfo);
 
       // intentMap.set('riasec-start-fallback', checkUncertainty);
       agent.handleRequest(intentMap);
