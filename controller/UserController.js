fa8a69c6f0d92973f0a7a8a6f7286fa5597877d3
reymond <reymondgomera9@gmail.com>
2022-08-27 12:00:35 +0800

    added userController and minor changes with the adminController

---
+++ controller/UserController.js
@@ -0,0 +1,40 @@
+const Conversation = require('../models/Conversation');
+const Course = require('../models/Course');
+
+const user_addConversation_post = async (req, res) => {
+   try {
+      const { name, age, sex, strand, riasec_code, riasec_course_recommendation, strand_course_recommendation } = req.body;
+      const newConversation = new Conversation({
+         name,
+         age,
+         sex,
+         strand,
+         riasec_code,
+         riasec_course_recommendation,
+         strand_course_recommendation,
+      });
+      const createdConversation = await newConversation.save();
+      res.json({ conversation: createdConversation, message: 'Conversation created successfully!' });
+   } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ message: 'Server Error' });
+   }
+};
+
+const user_fetchCourseByStrand_get = async (req, res) => {
+   try {
+      const { strand } = req.params;
+
+      const courses = await Course.find({ strand: { $in: strand } }).sort({ name: 'asc' });
+      const coursesNames = courses.map(course => course.name);
+      res.json({ courses: coursesNames });
+   } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ message: 'Server Error' });
+   }
+};
+
+module.exports = {
+   user_addConversation_post,
+   user_fetchCourseByStrand_get,
+};
--- controller/adminController.js
+++ controller/adminController.js
@@ -1,5 +1,6 @@
 const Feedback = require('../models/Feedback');
 const Course = require('../models/Course');
+const Conversation = require('../models/Conversation');
 
 const admin_addFeedback_post = async (req, res) => {
    try {
@@ -19,8 +20,10 @@ const admin_getFeedbacks_get = async (req, res) => {
 
       // set default page and size if queries are empty
       if (!page) page = 1;
-      if (!size) size = 10;
+      if (!size) size = 0;
       if (!search) search = '';
+      if (!sort) sort = 'createdAt';
+      if (!order) order = 'desc';
 
       const limit = parseInt(size);
       const skip = (parseInt(page) - 1) * size; // e.g. page = 2, size = 10 -> skip = 10
@@ -55,7 +58,47 @@ const admin_addCourse_post = async (req, res) => {
 const admin_getCourses_get = async (req, res) => {
    try {
       const courses = await Course.find();
-      res.json(courses);
+      res.json({ courses });
+   } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ message: 'Server Error' });
+   }
+};
+
+const admin_getConversations_get = async (req, res) => {
+   try {
+      let { page, size, search, sort, order } = req.query;
+
+      // set default page and size if queries are empty
+      if (!page) page = 1;
+      if (!size) size = 0;
+      if (!search) search = '';
+      if (!sort) sort = 'createdAt';
+      if (!order) order = 'desc';
+
+      const limit = parseInt(size);
+      const skip = (parseInt(page) - 1) * size; // e.g. page = 2, size = 10 -> skip = 10
+      let sortText = '';
+
+      if (sort && order) sortText = order !== 'desc' ? sort : `-${sort}`; // with dash(-) means sort in descending
+
+      const conversations = await Conversation.find({ name: { $regex: search, $options: 'i' } })
+         .limit(limit)
+         .skip(skip)
+         .sort(sortText);
+      const totalConversations = await Conversation.countDocuments({ name: { $regex: search, $options: 'i' } });
+      res.json({ page: parseInt(page), size: parseInt(size), total: totalConversations, conversations });
+   } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ message: 'Server Error' });
+   }
+};
+
+const admin_getConversation_get = async (req, res) => {
+   try {
+      const { conversationId } = req.params;
+      const conversation = await Conversation.findById(conversationId);
+      res.json({ conversation });
    } catch (err) {
       console.error(err.message);
       res.status(500).json({ message: 'Server Error' });
@@ -67,4 +110,6 @@ module.exports = {
    admin_getCourses_get,
    admin_addFeedback_post,
    admin_addCourse_post,
+   admin_getConversations_get,
+   admin_getConversation_get,
 };
