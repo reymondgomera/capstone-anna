29f519942fe4f6782420f75153b52a086bc2a7d8
reymond <reymondgomera9@gmail.com>
2022-11-02 14:16:38 +0800

    added additional environment variables

--- config/prod.js
+++ config/prod.js
@@ -6,4 +6,6 @@ module.exports = {
    google_private_key: JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
    mongo_URI: process.env.MONGO_URI,
    jwt_secret: process.env.JWT_SECRET,
+   admin_google_client_email: process.env.ADMIN_GOOGLE_CLIENT_EMAIL,
+   admin_google_private_key: process.env.ADMIN_GOOGLE_PRIVATE_KEY,
 };
